05f8d62d21e93f3d74ec16846ca8e302756a54bc
João Marcos <joao.santos160@fatec.sp.gov.br>
2020-11-26 00:54:53 -0300

    Ajustes pra funcionar localhost

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.129/api/contatos",
+  // baseURL: "http://192.168.0.129/api/contatos",
+  baseURL: "http://127.0.0.1:8000/api/contatos",
 });
 
 export const apiViaCep = axios.create({
